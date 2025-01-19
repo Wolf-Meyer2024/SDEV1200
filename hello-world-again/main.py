ce5d872600d1d81691e88c56675c4b831f276987
Wolf-Meyer2024 <wolfmeyer@westernwyoming.edu>
2025-01-18 18:03:19 -0700

    Update main.py

--- hello-world-again/main.py
+++ hello-world-again/main.py
@@ -1,8 +1,11 @@
 #
-# Name
-# Date
-# Hello Word! Again
-# SDEV 1200 
-#
+# Name: Wolfgang Meyer
+# Date: 1-15-2025
+# Project Name: HelloWorld_Again.py
+#SDEV1200
 
-# Use comments liberally throughout the program.
+# Assign the literal string to the variable
+greeting = "Hello World!"
+
+# Print the variable using an f-string
+print(f"{greeting}")
