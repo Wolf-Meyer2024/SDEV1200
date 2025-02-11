4e61fe1bc086531f7212d9e4cad79bdd8b7d2e54
Wolf-Meyer2024 <wolfmeyer@westernwyoming.edu>
2025-02-10 18:20:10 -0700

    Update person.py

--- person-and-customer-classes/person.py
+++ person-and-customer-classes/person.py
@@ -1,12 +1,22 @@
 #
-# Name
-# Date
+# Name Wolfgang Meyer
+# Date 2 - 10 - 25
 # Person and Customer Classes Programming Project
 # SDEV 1200
 #
 
 # Use comments liberally throughout the program.
 
-class Person:
+# person.py
 
-class Customer(Person):
+class Person:
+    # Initialize the Person class with name, address, and telephone number
+    def __init__(self, name, address, telephone):
+        self.name = name
+        self.address = address
+        self.telephone = telephone
+
+    # Return a string representation of the person
+    def __str__(self):
+        return f"Name: {self.name}\nAddress: {self.address}\nTelephone: {self.telephone}"
+
