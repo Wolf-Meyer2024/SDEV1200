005110b17b51b8e191baa2172d93f558a99f6b06
Wolf-Meyer2024 <wolfmeyer@westernwyoming.edu>
2025-03-13 05:05:56 -0600

    Update main.py

--- shiftsupervisor-class/main.py
+++ shiftsupervisor-class/main.py
@@ -1,7 +1,18 @@
 #
-# Name
-# Date
+# Name Wolfgang Meyer
+# Date 3-13-25
 # ShiftSupervisor Class Programming Project
 # SDEV 1200
 #
-#Use comments liberally throughout the program.
+# Use comments liberally throughout the program.
+
+class ShiftSupervisor(Employee):
+    def __init__(self, name, id_number, department, job_title):
+        super().__init__(name, id_number, department, job_title)
+        self.annual_salary = annual_salary
+        self.annual_bonus = annual_bonus
+    def display_info(self):
+        base_info = super().display_info()
+        print (f"annual salary: ${self.annual_salary: .2f}")
+        print (f"annual bonus: ${self.annual_bonus: .2f}")
+        print ("-" * 40)
