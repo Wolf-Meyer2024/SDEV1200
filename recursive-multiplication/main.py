fa29026ae93a402a8c0c0837dc0d95782e8e0907
Wolf-Meyer2024 <wolfmeyer@westernwyoming.edu>
2025-03-17 04:08:54 -0600

    Update main.py

--- recursive-multiplication/main.py
+++ recursive-multiplication/main.py
@@ -1,7 +1,16 @@
 #
-# Name
-# Date
+# Name Wolfgang Meyer
+# Date 3-17-25
 # Recursive Multiplication Programming Project
 # SDEV 1200
 #
 # Use comments liberally throughout the program. 
+
+def recursive_multiplication(x, y):
+    if x == 1:
+        return y
+    else:
+        return y + recursive_multiplication(x - 1, y)
+    
+print(recursive_multiplication(7, 4))  # Expected output: 28
+print(recursive_multiplication(3, 5))  # Expected output: 15
